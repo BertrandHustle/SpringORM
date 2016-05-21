b087209ebb57ee615f3c25c75a8f783fd8c7f35d
BertrandHustle <scott.greenberg926@gmail.com>
2016-05-21 16:41:54 -0400

    successful connection

--- src/main/java/com/theironyard/SpringORMController.java
+++ src/main/java/com/theironyard/SpringORMController.java
@@ -2,6 +2,11 @@ package com.theironyard;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class SpringORMController {
@@ -11,5 +16,18 @@ public class SpringORMController {
 
     @Autowired
     PurchaseRepo purchaseRepo;
+
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String webRoot(Model model, HttpSession session){
+
+        //used to add purchases to model
+        Iterable<Purchase> purchases;
+        purchases = purchaseRepo.findAll();
+
+        model.addAttribute("purchases", purchases);
+        return "home";
+
+    }
 
 }
